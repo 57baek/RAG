427fbe0dd2d16d404b5a2a614c52d7e1c65eb921
HyangMok Baek <57baek@gmail.com>
2025-05-01 20:15:43 -0400

    14th commit

--- app/models/chatting_model.py
+++ app/models/chatting_model.py
@@ -4,5 +4,5 @@ from langchain_openai import ChatOpenAI
 def get_chatting_model_openai():
     return ChatOpenAI(
         model="gpt-4o-mini",
-        # Options: "gpt-4o-mini", "gpt-4o", "gpt-3.5-turbo",
+        # Options: "gpt-4o-mini", "gpt-4o", "gpt-3.5-turbo"
     )
